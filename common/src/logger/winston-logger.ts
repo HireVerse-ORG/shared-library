d26c56a274009733ca02cc756db73cec39e982b4
rahiii-dev <rahiofcl@gmail.com>
2024-12-20 18:30:16 +0530

    fix(common): logger update

--- common/src/logger/winston-logger.ts
+++ common/src/logger/winston-logger.ts
@@ -1,19 +1,24 @@
 import winston from "winston";
-import LogstashTransport  from "winston-logstash/lib/winston-logstash-latest.js";
+import LogstashTransport from "winston-logstash/lib/winston-logstash-latest.js";
 
-export const createLogger = (level: string, logstash = {host: 'localhost', port: 5044}) => {
+export const createLogger = (level: string, enableLogstash: boolean, logstash = {host: 'localhost', port: 5044}) => {
+    const transports = [
+        new winston.transports.Console(),
+    ];
+
+    if (enableLogstash) {
+        transports.push(new LogstashTransport({
+            ...logstash,
+            maxConnectRetries: 3,
+            retryInterval: 5000,
+            serializer: winston.format.json()
+        }));
+    }
+
     return winston.createLogger({
         level: level,
-        transports: [
-            new winston.transports.Console(),
-            new LogstashTransport({
-                ...logstash,
-                maxConnectRetries: 3,
-                retryInterval: 5000,
-                serializer: winston.format.json()
-            }),
-        ],
+        transports: transports,
     });
 };
 
-export const customLogger = createLogger('info');
+export const customLogger = createLogger('info', false);
