375429ffd02da62c6ef8d14e3195c5909bf4ce3e
rahiii-dev <rahiofcl@gmail.com>
2025-02-17 22:56:39 +0530

    fix: user event updated

--- kafka-communication/src/events/userEvents.ts
+++ kafka-communication/src/events/userEvents.ts
@@ -2,9 +2,11 @@ import { CustomMessage, ServerEvent } from "../types";
 import { KafkaTopics } from "./topics";
 
 export interface UserCreatedMessage {
-    id: string;
+    userId: string;
     email: string;
     fullName: string;
+    role: string;
+    timeStamp: Date;
 }
 
 export function UserCreatedEvent(data: CustomMessage<UserCreatedMessage>): ServerEvent<UserCreatedMessage> {
